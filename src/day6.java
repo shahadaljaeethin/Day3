463e660c0dab6647691eaf5e1ba416889245cc4f
shahadaljaeethin <shahadaljaeethin@gmail.com>
2025-10-19 11:18:58 +0300

    d6

--- src/day6.java
+++ src/day6.java
@@ -10,28 +10,35 @@ public void main(String[] args){
     for(int i=0;i<sisters.length;i++)  System.out.println(sisters[i]);
     for(int i=0;i<cars.length;i++)  System.out.println(cars[i]);
     System.out.println(Arrays.toString(sisters));
-String name ="name";
 
-
-    for(int i =0;i<numbers.length;i++){
-        if(numbers[i]%2==0)
-            System.out.println(numbers[i]+" is even.");
-        else  System.out.println(numbers[i]+" is odd.");
-
-    }
-   // int lastIndex=0;
+//
+//    for(int i =0;i<numbers.length;i++){
+//        if(numbers[i]%2==0)
+//            System.out.println(numbers[i]+" is even.");
+//        else  System.out.println(numbers[i]+" is odd.");
+//
+//    }
+//   // int lastIndex=0;
     String[] names = {"Sara","Majd","Sameer","Ahmed","Suha"};
-    for(int i=0;i<names.length;i++){
-//        lastIndex = names[i].length()-1;
-//        System.out.println(names[i].charAt(lastIndex)+",");
-        if(names[i].charAt(0)=='S')
-            System.out.print(names[i]+" ");
-    }
-    System.out.print("\n");
+//    for(int i=0;i<names.length;i++){
+////        lastIndex = names[i].length()-1;
+////        System.out.println(names[i].charAt(lastIndex)+",");
+//        if(names[i].charAt(0)=='S')
+//            System.out.print(names[i]+" ");
+//    }
+//    System.out.print("\n");
+//names[0]+=",Edit";
+  //  System.out.print(names[0]+"\n");
+for(int i:numbers){
+    if(i%2==0)  System.out.println(i+" is even.");
+else  System.out.println(i+" is odd.");
 
-
-
-
+}
+for(String n: names)
+{
+    if(n.charAt(0)!='S') continue;
+    System.out.print(n+", ");
+}
 
 }
 }
